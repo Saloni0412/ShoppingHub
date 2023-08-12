1b8d078526c3f9c19cba8dcf66ed393fc4b4b286
PacoCasillas <pacocasillasmx@gmail.com>
2023-08-12 16:14:26 -0400

    Created node server + installed dependencies

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+
+// This route will return all the products in the data.js file.
+app.get('/api/products', (req, res) => {
+    res.send(data.products);
+});
+
+const port = process.env.PORT || 5000;
+
+// This line listens on port 5000, or the port specified in the PORT environment variable.
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+});
+
