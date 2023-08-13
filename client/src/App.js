89ac6098ce79ae6b3252a28e7fc6f17ef7edf937
Saloni0412 <pshashikantb@gmail.com>
2023-08-13 17:56:34 -0400

    this adds code in checkoutsteps.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/product/:slug" element={<ProductScreen />} />
+            <Route path="/shipping" element={<ShippingAddressScreen />} />
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         </main>
