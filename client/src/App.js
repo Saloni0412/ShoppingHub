9f3a66b2fb6be9a9bfd042c10a0a6b4c9ef0e7f6
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-08-13 23:38:17 -0400

    create and implement react context

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import ShippingAddressScreen from './screens/ShippingAddressScreen';
-import SigninScreen from './screens/SigninScreen';
-
-import Navbar from 'react-bootstrap/Navbar'
-import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap';
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import SigninScreen from "./screens/SigninScreen";
+import Badge from "react-bootstrap/Badge";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+import { useContext } from "react";
+import { Store } from "./Store";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
         <header>
-          <navbar bg="dark" variant="dark">
-             <Container>
+          <Navbar bg="dark" variant="dark">
+            <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>ShoppingHub</Navbar.Brand>
               </LinkContainer>
-             </Container>
-          </navbar>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.length}
+                    </Badge>
+                  )}
+                </Link>
+              </Nav>
+            </Container>
+          </Navbar>
         </header>
         <main>
           <Container className="mt-3">
