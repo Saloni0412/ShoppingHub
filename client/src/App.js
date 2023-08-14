0b2a68085fd2cc1e40376e5de6d54af01a5cf6b4
hiroro <hiroaki.a1989@gmail.com>
2023-08-14 11:16:19 +0900

    to show product detailed page

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ function App() {
           </navbar>
         </header>
         <main>
-          <Container>
+          <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -2,7 +2,13 @@ import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import logger from  'use-reducer-logger';
+import { Helmet } from 'react-helmet-async';
 //import data from "../data";
+
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from '../components/Product';
+
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -41,6 +47,9 @@ function HomeScreen() {
   }, []);
   return (
     <div>
+      <Helmet>
+        <title>ShoppingHub</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
         {loading ? (
@@ -48,22 +57,14 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              // 6/12 => showing 2 in a row
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
