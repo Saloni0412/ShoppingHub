6516b6cc25b88020004fc1750a8d9200db47a68d
hiroro <hiroaki.a1989@gmail.com>
2023-08-15 16:08:27 +0900

    added products page, rendered from backend

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -6,6 +6,7 @@ import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
+import { Helmet } from 'react-helmet-async';
 
 
 const reducer = (state, action) => {
@@ -41,6 +42,9 @@ function HomeScreen() {
     }, []);
   return (
     <div>
+      <Helmet>
+        <title>ShoppingDepo</title>
+      </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
       {loading ? (
