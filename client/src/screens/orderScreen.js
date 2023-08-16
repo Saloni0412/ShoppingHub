07bf5202b2548a7c7a76fabd50007f1dd2dea74e
hiroro <hiroaki.a1989@gmail.com>
2023-08-16 15:47:05 +0900

    fixed bug

--- client/src/screens/orderScreen.js
+++ client/src/screens/orderScreen.js
@@ -1,24 +1,24 @@
-import axios from 'axios';
-import React, { useContext, useEffect, useReducer } from 'react';
-import { Helmet } from 'react-helmet-async';
-import { useNavigate, useParams } from 'react-router-dom';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import { Store } from '../Store';
-import { getError } from '../utils';
+import axios from "axios";
+import React, { useContext, useEffect, useReducer } from "react";
+import { Helmet } from "react-helmet-async";
+import { useNavigate, useParams } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import { Store } from "../Store";
+import { getError } from "../utils";
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'FETCH_REQUEST':
-      return { ...state, loading: true, error: '' };
-    case 'FETCH_SUCCESS':
-      return { ...state, loading: false, order: action.payload, error: '' };
-    case 'FETCH_FAIL':
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, order: action.payload, error: "" };
+    case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
 
     default:
@@ -27,84 +27,69 @@ function reducer(state, action) {
 }
 
 export default function OrderScreen() {
-    const { state } = useContext(Store);
-    const { userInfo } = state;
-  
-    const params = useParams();
-    const { id: orderId } = params;
-    const navigate = useNavigate();
-  
-    const [{ loading, error, order }, dispatch] = useReducer(reducer, {
-      loading: true,
-      order: {},
-      error: '',
-    });
+  const { state } = useContext(Store);
+  const { userInfo } = state;
 
-    useEffect(() => {
-        const fetchOrder = async () => {
-          try {
-            dispatch({ type: 'FETCH_REQUEST' });
-            const { data } = await axios.get(`/api/orders/${orderId}`, {
-              headers: { authorization: `Bearer ${userInfo.token}` },
-            });
-            dispatch({ type: 'FETCH_SUCCESS', payload: data });
-          } catch (err) {
-            dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
-          }
-        };
-    
-        if (!userInfo) {
-          return navigate('/signin');
-        }
-        if (!order._id || (order._id && order._id !== orderId)) {
-            fetchOrder();
-          }
-        }, [order, userInfo, orderId, navigate]);
-        return loading ? (
-            <LoadingBox></LoadingBox>
-          ) : error ? (
-            <MessageBox variant="danger">{error}</MessageBox>
-          ) : (
-            <div>
-              <Helmet>
-                <title>Order {orderId}</title>
-              </Helmet>
-              <h1 className="my-3">Order {orderId}</h1>
-              <Row>
-                <Col md={8}>
-                  <Card className="mb-3">
-                    <Card.Body>
-                      <Card.Title>Shipping</Card.Title>
-                      <Card.Text>
-                        <strong>Name:</strong> {order.shippingAddress.fullName} <br />
-                        <strong>Address: </strong> {order.shippingAddress.address},
-                        {order.shippingAddress.city}, {order.shippingAddress.postalCode}
-                        ,{order.shippingAddress.country}
-                      </Card.Text>
-                      {order.isDelivered ? (
-                        <MessageBox variant="success">
-                          Delivered at {order.deliveredAt}
-                        </MessageBox>
-                      ) : (
-                        <MessageBox variant="danger">Not Delivered</MessageBox>
-                      )}
-                    </Card.Body>
-                  </Card>
-                  <Card className="mb-3">
+  const params = useParams();
+  const { id: orderId } = params;
+  const navigate = useNavigate();
+
+  const [{ loading, error, order }, dispatch] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: "",
+  });
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`/api/orders/${orderId}`, {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        });
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+
+    if (!userInfo) {
+      return navigate("/signin");
+    }
+    if (!order._id || (order._id && order._id !== orderId)) {
+      fetchOrder();
+    }
+  }, [order, userInfo, orderId, navigate]);
+  return loading ? (
+    <LoadingBox></LoadingBox>
+  ) : error ? (
+    <MessageBox variant="danger">{error}</MessageBox>
+  ) : (
+    <div>
+      <Helmet>
+        <title>Order {orderId}</title>
+      </Helmet>
+      <h1 className="my-3">Order {orderId}</h1>
+      <Row>
+        <Col md={8}>
+          <Card className="mb-3">
             <Card.Body>
-              <Card.Title>Payment</Card.Title>
+              <Card.Title>Shipping</Card.Title>
               <Card.Text>
-                <strong>Method:</strong> {order.paymentMethod}
+                <strong>Name:</strong> {order.shippingAddress.fullName} <br />
+                <strong>Address: </strong> {order.shippingAddress.address},
+                {order.shippingAddress.city}, {order.shippingAddress.postalCode}
+                ,{order.shippingAddress.country}
               </Card.Text>
-              {order.isPaid ? (
+              {order.isDelivered ? (
                 <MessageBox variant="success">
-                  Paid at {order.paidAt}
+                  Delivered at {order.deliveredAt}
                 </MessageBox>
               ) : (
-                <MessageBox variant="danger">Not Paid</MessageBox>
+                <MessageBox variant="danger">Not Delivered</MessageBox>
               )}
             </Card.Body>
-          </Card>
+          </Card>          
 
           <Card className="mb-3">
             <Card.Body>
@@ -134,7 +119,7 @@ export default function OrderScreen() {
                           src={item.image}
                           alt={item.name}
                           className="img-fluid rounded img-thumbnail"
-                        ></img>{' '}
+                        ></img>{" "}
                         <Link to={`/product/${item.slug}`}>{item.name}</Link>
                       </Col>
                       <Col md={3}>
@@ -158,7 +143,7 @@ export default function OrderScreen() {
                   <Row>
                     <Col>Items</Col>
                     <Col>${order.itemsPrice.toFixed(2)}</Col>
-                    </Row>
+                  </Row>
                 </ListGroup.Item>
                 <ListGroup.Item>
                   <Row>
@@ -190,6 +175,3 @@ export default function OrderScreen() {
     </div>
   );
 }
-
-          
-                  
